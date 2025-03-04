2c34e13b7e0db17d5e1f146ced3a0d31e976bec2
yeeun234 <wooyeeun2@gmail.com>
2025-03-04 14:50:14 +0900

    로또번호생성

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { IoHome } from "react-icons/io5";
 import { GrAndroid } from "react-icons/gr";
 // import MyClock from "./02/Myclock";
 // import MyDiv1 from "./03/MyDiv1";
-import MyList from "./04/MyList";
+// import MyList from "./04/MyList";
+import Lotto from "./05/Lotto";
 
 
 function App() {
@@ -19,10 +20,7 @@ function App() {
       </header>
       <main className="w-full flex-grow text-center
       flex flex-col justify-center items-center overflow-y-auto">
-       {/* <Hello/> */}
-        {/* <MyClock/> */}
-        {/* <MyDiv1/> */}
-        <MyList/>
+       <Lotto></Lotto>
       </main>
       <footer className="w-full h-10 bg-pink-100 flex justify-center items-center px-10">
         <h1 className="font-bold text-[6px] text-gray-600">[K-Digital 부산대 25-1회차] AI 데이터 분석 풀스택 웹 개발자 양성과정</h1>
