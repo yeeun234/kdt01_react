d598b905b6805352fa36310a6ed2b8fd9be3f905
yeeun234 <wooyeeun2@gmail.com>
2025-03-11 12:53:46 +0900

    daily boxoffice fetch

---
+++ src/09/BoxOffice.jsx
@@ -0,0 +1,12 @@
+import { useState,useEffect } from "react"
+
+export default function BoxOffice() {
+    const [tags,setTags] = useState([]);
+
+    useEffect(()=>{},[]);
+  return (
+    <div>
+      v
+    </div>
+  )
+}
