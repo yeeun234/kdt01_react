347f09eca668308d0f1bb6756dd9607b0bc35cbe
yeeun234 <wooyeeun2@gmail.com>
2025-03-11 12:57:13 +0900

    getfetch

--- src/09/BoxOffice.jsx
+++ src/09/BoxOffice.jsx
@@ -1,9 +1,19 @@
 import { useState,useEffect } from "react"
 
 export default function BoxOffice() {
+    //the variable which will be rendered in screen
     const [tags,setTags] = useState([]);
 
-    useEffect(()=>{},[]);
+    //take the daily boxoffice data
+    const getFetchData = async () =>{
+        let url =''
+        url =`${url}`
+    } 
+
+    useEffect(()=>{
+        //will fetch once when component was run.
+        getFetchData();
+    },[]);
   return (
     <div>
       v
