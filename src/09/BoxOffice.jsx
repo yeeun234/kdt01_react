464093f5b1cc19fff80fa10307fa0da5820e95d8
yeeun234 <wooyeeun2@gmail.com>
2025-03-12 11:01:38 +0900

    영화순위 패치

--- src/09/BoxOffice.jsx
+++ src/09/BoxOffice.jsx
@@ -1,30 +1,118 @@
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
 
 export default function BoxOffice() {
-    //the variable which will be rendered in screen
-    const [tags,setTags] = useState([]);
+  //State variables
+  const [tags, setTags] = useState([]);
 
-    //take the daily boxoffice data
-    const getFetchData = async () =>{
-        let url ='https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?'
-        url =`${url}key==20250310`;
-        console.log(url)
+ //어제날짜가져오기 
+ const getYesterday = () => {
+  let dt = new Date();
+  dt.setDate(dt.getDate() - 1);
 
-        const resp = await fetch(url);
-        const data = await resp.json();
+  //년도
+  let year = dt.getFullYear();
 
-        let boxList = data.boxOfficeResult.dailyBoxOfficeList ;
-        console.log(boxList);
-    } 
+  //월
+  let month = String(dt.getMonth() + 1).padStart(2, '0');
+  // month = month < 10 ? '0' + month : month ;
 
-    useEffect(()=>{
-        //will fetch once when component was run.
-        getFetchData();
-        
-    },[]);
+  //일 
+  let day = String(dt.getDate()).padStart(2, '0');
+
+  return (year + '-' + month + '-' + day);
+}
+
+
+  //fetch the daily boxoffice data
+  const getFetchData = async () => {
+    const mvApiKey = import.meta.env.VITE_APP_MY_KEY;
+    let dt = getYesterday().replaceAll('-', '');
+    let url = 'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?'
+    url = `${url}key=${mvApiKey}&targetDt=${dt}`;
+    console.log(url)
+
+    const resp = await fetch(url);
+    const data = await resp.json();
+
+    let boxList = data.boxOfficeResult.dailyBoxOfficeList;
+    console.log(boxList);
+
+    let tm = boxList.map(item => 
+                                //맵의 리턴값은 하나여야하니 tr 태그로 감싸주기 , 키값은 tr태그 안에 속성으로 넣어 html 에서 안보이게 하기.
+                                <tr key={item.movieCd}  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <td className="w-4 p-4">
+                                  
+                                </td>
+                                <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {item.rank}
+                                </td>
+                                <td className="px-6 py-4">
+                                {item.movieNm}
+                                </td>
+                                <td className="px-6 py-4">
+                                {item.salesAmt}
+                                </td>
+                                <td className="px-6 py-4">
+                                {item.audiCnt}
+                                </td>
+                                <td className="px-6 py-4">
+                                  {item.salesAcc}
+                                </td>
+                                <td className="px-6 py-4">
+                                  {item.audiAcc}
+                                </td>
+                                <td className="px-6 py-4">
+                                  {item.rankInten}
+                                </td>
+                              </tr>  
+                              
+        );
+
+    setTags(tm);
+  }
+
+
+
+  useEffect(() => {
+    //will fetch once when component was run.
+    getFetchData();
+
+  }, []);
   return (
     <div>
-      v
+     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <td className="p-4">              
+              </td>
+              <td className="px-6 py-3">
+                순위
+              </td>
+              <th className="px-6 py-3">
+                영화명
+              </th>
+              <th className="px-6 py-3">
+                매출액
+              </th>
+              <th className="px-6 py-3">
+                관객수
+              </th>
+              <th className="px-6 py-3">
+                누적 매출액
+              </th>
+              <th className="px-6 py-3">
+                누적 관객수
+              </th>
+              <th className="px-6 py-3">
+                증감율
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {tags}
+          </tbody>
+        </table>
+        
     </div>
   )
 }
