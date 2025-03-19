63744b8c0d30cd83df35014d2455da8aa4a7fb88
yeeun234 <wooyeeun2@gmail.com>
2025-03-19 12:37:43 +0900

    검색창 오토포커스 *유즈레퍼,유즈이펙트를 이용

--- src/12/GalleryNav.jsx
+++ src/12/GalleryNav.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
 import TailButton from '../UI/TailButton'
+import { useEffect ,useRef } from 'react'
+
 
 export default function GalleryNav() {
+    const inputk = useRef();
+    useEffect(()=>{
+        inputk.current.focus();
+    },[]);
+
     return (
         <div className='w-8/10 h-full flex-col justify-center items-center mt-5 bg-gray-50'> 
             <h1 className='text-2xl font-bold inline-flex justify-center items-center mt-10'>한국관광공사 관광 사진 정보</h1>
             <div className='flex justify-center items-center w-full h-15   m-2 '>
-                <input className="bg-white rounded-xs w-5/10 h-10/12 mr-2" type="text" autoFocus />
+                <input ref={inputk} className=" border 1px soild bg-white rounded-xs w-5/10 h-10/12 mr-2" type="text" autoFocus />
                 <TailButton caption="확인" color="blue" />
                 <TailButton caption="취소" color="blue" />
             </div>
