3db7f982c164d3609c23851edb1f07c79ee12b54
yeeun234 <wooyeeun2@gmail.com>
2025-03-19 12:05:34 +0900

    검색레이아웃 수정 : app.jsx justyfy start (위에서 부터 시작) / 그리드는 place-items-center , h-20은 h-20px을 뜻하는게 아님

--- src/12/GalleryNav.jsx
+++ src/12/GalleryNav.jsx
@@ -4,8 +4,9 @@ import TailButton from '../UI/TailButton'
 export default function GalleryNav() {
   return (
     <div className='flex justify-center items-center w-full h-15  bg-amber-300 m-10'>
-      <input className="bg-white rounded-xs w-5/10 h-10/12 " type="text" autoFocus/>
-        <TailButton caption="검색" color="blue" />
+      <input className="bg-white rounded-xs w-5/10 h-10/12 mr-2" type="text" autoFocus/>
+        <TailButton caption="확인" color="blue" />
+        <TailButton caption="취소" color="blue" />
     </div>
   )
 }
