284d3bea54d049ea80ec4e48a02afb56f4ecc6ad
yeeun234 <wooyeeun2@gmail.com>
2025-03-20 13:02:56 +0900

    축제카드 /셀렉트 박스 레이아웃 잡기 (마진/그리드)

--- src/UI/TailSelect.jsx
+++ src/UI/TailSelect.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 export default function TailSelect({ id, selectRef, handleChange , optionArray }) {
     return (
         <div>
-            <select id={id} ref={selectRef} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+            <select id={id} ref={selectRef} onChange={handleChange} className="mb-2 mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                  {
                 optionArray.map((item,idx) => {
                     return <option value={item} key={item+idx}>{item}</option>
