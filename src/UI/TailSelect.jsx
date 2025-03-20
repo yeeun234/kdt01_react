02d308dd3b7061065ab00acd18ba3b0f1c9ce621
yeeun234 <wooyeeun2@gmail.com>
2025-03-20 12:02:08 +0900

    중복되는게 있을시 인덱스값을 추가로 넣어 중복키제거

--- src/UI/TailSelect.jsx
+++ src/UI/TailSelect.jsx
@@ -5,8 +5,8 @@ export default function TailSelect({ id, selectRef, handleChange , optionArray }
         <div>
             <select id={id} ref={selectRef} onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                 {
-                optionArray.map((item) => {
-                    return <option value={item} key={item}>{item}</option>
+                optionArray.map((item,idx) => {
+                    return <option value={item} key={item+idx}>{item}</option>
                 })}
             </select>
         </div>
