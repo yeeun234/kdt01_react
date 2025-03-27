dce723a6c95531c18ff45602e24dd9d11133646a
yeeun234 <wooyeeun2@gmail.com>
2025-03-27 11:21:57 +0900

    opsSi 배열에 시도선택 넣어서 선택박스에 넣기

--- src/15/Fcst.jsx
+++ src/15/Fcst.jsx
@@ -3,17 +3,19 @@ import TailSelect from '../UI/TailSelect';
 import TailButtonLine from '../UI/TailButtonLine';
 import getxy from './getxy.json';
 export default function Fcst() {
-    const Ref = useRef();
+    const RefDate = useRef();
+    const RefSi = useRef();
 
+    let opsSi = getxy.map(item=>item["1단계"]);
+    opsSi=["--시/도 선택--" ,...opsSi];
   return (
     <>
         <h1 className="m-6 font-bold text-3xl">날씨 정보</h1>
         <form onSubmit={(e)=>{e.preventDefault()}} className='w-10/12 grid grid-cols-1 lg:grid-cols-2 gap 4 border 1px solid '>
-            <input type='date' className=' m-3'  />
-            <TailSelect id="sel" selectRef={Ref} handleChange={()=>{}} optionArray={["부산"]} />
+            <input ref={RefDate} type='date' className=' m-3'  />
+            <TailSelect id="sel" selectRef={RefSi} handleChange={()=>{}} optionArray={opsSi} />
             <TailButtonLine html="초단기예보" bgColor="blue" onClick={()=>{}}/>
             <TailButtonLine html="단기예보" bgColor="blue" onClick={()=>{}}/>
-        
         </form>
     </>
   )
