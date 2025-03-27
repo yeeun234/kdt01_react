332e5d2c7b0067426e77b219717ad45f53dc647f
yeeun234 <wooyeeun2@gmail.com>
2025-03-27 11:54:13 +0900

    유즈서치파람즈의 매개변수 배열이 아니라 겟 메소드로 받기

--- src/15/FcstList.jsx
+++ src/15/FcstList.jsx
@@ -1,18 +1,22 @@
 import { useSearchParams } from "react-router-dom"
 import { useRef  } from 'react';
 
+
 import TailSelect from "../UI/TailSelect";
 export default function FcstList() {
   const [ser] = useSearchParams();
-  console.log("ser",ser);
-  let qlist = [...ser];
-  console.log("qlist",qlist);
-  let date = qlist[0][1];
-  let si = qlist[1][1]; 
-  let guban = qlist[2][1];
-  let x = qlist[3][1];
-  let y = qlist[4][1];
+  const date = ser.get('dt');
+  const si = ser.get('si'); 
+  const guban = ser.get('guban');
+  const x = ser.get('x');
+  const y = ser.get('y');
+
   const Ref = useRef();
+
+  const getFetch = async () =>{
+    const myKey = import.meta.env.VITE_APP_MY_KEY;
+    let url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${myKey}&pageNo=1&numOfRows=1000&dataType=json&base_date=20250327&base_time=0500&nx=55&ny=127`;
+  }
   return (
     <div>
        <h1 className="m-6 font-bold text-xl ">
