8b854ae1c95574ef246f2352a7fa853a867b1b04
yeeun234 <wooyeeun2@gmail.com>
2025-03-27 11:24:24 +0900

    handleChange() *셀렉트 박스 선택시 RefSi의 벨류값 콘솔출력

--- src/15/Fcst.jsx
+++ src/15/Fcst.jsx
@@ -8,12 +8,17 @@ export default function Fcst() {
 
     let opsSi = getxy.map(item=>item["1단계"]);
     opsSi=["--시/도 선택--" ,...opsSi];
+
+    
+    const handleChange = () => {
+        console.log(RefSi.current.value)
+    }
   return (
     <>
         <h1 className="m-6 font-bold text-3xl">날씨 정보</h1>
         <form onSubmit={(e)=>{e.preventDefault()}} className='w-10/12 grid grid-cols-1 lg:grid-cols-2 gap 4 border 1px solid '>
             <input ref={RefDate} type='date' className=' m-3'  />
-            <TailSelect id="sel" selectRef={RefSi} handleChange={()=>{}} optionArray={opsSi} />
+            <TailSelect id="sel" selectRef={RefSi} handleChange={handleChange} optionArray={opsSi} />
             <TailButtonLine html="초단기예보" bgColor="blue" onClick={()=>{}}/>
             <TailButtonLine html="단기예보" bgColor="blue" onClick={()=>{}}/>
         </form>
