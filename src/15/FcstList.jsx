078c30d5f32eb8f9153eebe7ce3f4d80bea27eb1
yeeun234 <wooyeeun2@gmail.com>
2025-03-27 12:29:48 +0900

    getcode import 후 opsItems 에 예보구분이 gubun이랑 같은 것들을 필터로 뽑아 배열로 만들고 그 배열을 값이 항목명과 항목값이 동시에 나오는 배열로 다시 만들기.
    opsItems 가 일렬로 나온건  optionArray={opsItems}를  optionArray={[opsItems]} 로 설정해둬서 그랬음

--- src/15/FcstList.jsx
+++ src/15/FcstList.jsx
@@ -1,28 +1,41 @@
 import { useSearchParams } from "react-router-dom"
-import { useRef  } from 'react';
+import { useRef } from 'react';
+import getcode from './getcode.json';
 
 
 import TailSelect from "../UI/TailSelect";
 export default function FcstList() {
   const [ser] = useSearchParams();
   const date = ser.get('dt');
-  const si = ser.get('si'); 
-  const guban = ser.get('guban');
+  const si = ser.get('si');
+  const guban = ser.get('gubun');
   const x = ser.get('x');
   const y = ser.get('y');
 
-  const Ref = useRef();
+  const RefItems = useRef();
+  const opsItems =   getcode.filter(item=>item["예보구분"] == guban)
+                            .map(item=>`${item["항목명"]}(${item["항목값"]})`);
+  console.log(guban, opsItems);
+  const handleChange = () => {
+    console.log(RefItems.current.value)
+    console.log(opsItems);
+  }
 
-  const getFetch = async () =>{
+  const getFetch = async () => {
     const myKey = import.meta.env.VITE_APP_MY_KEY;
     let url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${myKey}&pageNo=1&numOfRows=1000&dataType=json&base_date=20250327&base_time=0500&nx=55&ny=127`;
   }
+
+  
   return (
     <div>
-       <h1 className="m-6 font-bold text-xl ">
-        {si} {guban} ( {date} )
-       </h1>
-       <TailSelect id="sel" selectRef={Ref} handleChange={()=>{}} optionArray={[]}/>
+
+      <div className='w-10/12 grid grid-cols-1 lg:grid-cols-2 gap 4 border 1px solid '>
+        <h1 className="m-6 font-bold text-xl ">
+          {si} {guban} ( {date.replaceAll('-', '.')} )
+        </h1>
+        <TailSelect id="sel" selectRef={RefItems} handleChange={handleChange} optionArray={opsItems} />
+      </div>
     </div>
   )
 }
