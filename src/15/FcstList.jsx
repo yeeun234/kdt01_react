3ddc5823f844e8367a5c1317df5a088196a359da
yeeun234 <wooyeeun2@gmail.com>
2025-03-27 11:46:54 +0900

    useSearchParams을 이용해 네비게이트로 받은 변수값 추출

--- src/15/FcstList.jsx
+++ src/15/FcstList.jsx
@@ -1,8 +1,24 @@
+import { useSearchParams } from "react-router-dom"
+import { useRef  } from 'react';
 
+import TailSelect from "../UI/TailSelect";
 export default function FcstList() {
+  const [ser] = useSearchParams();
+  console.log("ser",ser);
+  let qlist = [...ser];
+  console.log("qlist",qlist);
+  let date = qlist[0][1];
+  let si = qlist[1][1]; 
+  let guban = qlist[2][1];
+  let x = qlist[3][1];
+  let y = qlist[4][1];
+  const Ref = useRef();
   return (
     <div>
-      FcstList
+       <h1 className="m-6 font-bold text-xl ">
+        {si} {guban} ( {date} )
+       </h1>
+       <TailSelect id="sel" selectRef={Ref} handleChange={()=>{}} optionArray={[]}/>
     </div>
   )
 }
